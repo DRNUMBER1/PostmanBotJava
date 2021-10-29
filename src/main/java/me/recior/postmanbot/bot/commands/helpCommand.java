5138d0b268e375bdc5f80c269891c1bdc156058a
Rykun <66620681+DRNUMBER1@users.noreply.github.com>
2021-10-29 15:32:31 +0530

    idk but works...

--- src/main/java/me/recior/postmanbot/bot/commands/helpCommand.java
+++ src/main/java/me/recior/postmanbot/bot/commands/helpCommand.java
@@ -40,8 +40,8 @@ public class helpCommand extends Command {
         EmbedBuilder eb = new EmbedBuilder();
 //embed titles
         eb.setTitle("Postman Bot help Menu.");
-        eb.setDescription("Welcome to Postman Bot help Menu!, All my commands Start with ");
-        eb.addField("Commands\n",result + "``",false);
+        eb.setDescription("Welcome to Postman Bot help Menu!, All my commands Start with +");
+        eb.addField("Commands\n", "``+" + result + "``",false);
 
 
        textChannel.sendMessageEmbeds(eb.build()).queue();
