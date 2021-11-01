7b86e1a2cd62c4b090c17a676b9c3d7f9a21bd9e
FlufferyΕΛ <74445505+FlufferyGR@users.noreply.github.com>
2021-11-01 08:59:28 -0400

    Who asked tho

--- src/main/java/me/recior/postmanbot/bot/commands/fun/whoasked.java
+++ src/main/java/me/recior/postmanbot/bot/commands/fun/whoasked.java
@@ -3,5 +3,36 @@ package me.recior.postmanbot.bot.commands.fun;
 import me.recior.postmanbot.bot.Command;
 import me.recior.postmanbot.bot.util.Cmd;
 
-// i made this at school 
-// xd
+
+/*
+*
+* how many people asked about your stupid opinion?
+* I made this during school, i find it funny
+*
+* */
+public class size extends Command {
+
+    //registering....
+    public size() {
+        super("whoasked", "Shows how many people dont care >;)", new String[]{"people","long"});
+    }
+
+    //calling....
+    @Override
+    public void call(Cmd c){
+        String name = c.getMember().getEffectiveName();
+        int max = 15;
+        int min = 1;
+        int range = max - min + 1;
+
+        //why not?
+        for (int i = 0; i < 1; i++) {
+            int rand = (int) (Math.random() * range) + min;
+            if(rand < 7)
+                c.getChannel().sendMessage("Hey " + name + " not alot of people agree with you, onlu " + rand + "does // Lmao, not based.").queue();
+            else {
+                c.getChannel().sendMessage("Hey " + name + " people agree and theyre " + rand + "// very based fellow i see.").queue();
+            }
+        }
+    }
+}
